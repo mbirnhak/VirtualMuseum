d8f4e5b9e002391167225ba095058526b492a21f
Matthew Birnhak <MattBirnhak@MacBook-Pro-63.local>
2024-12-03 18:39:11 -0500

    basic frontend working

--- frontend/App.py
+++ frontend/App.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
